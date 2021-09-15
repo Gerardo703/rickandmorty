c25ad3cded3626607ca94d20ffae9f185cc6bf00
Gerardo Vega <54243381+Gerardo703@users.noreply.github.com>
2021-09-15 13:53:10 -0300

    Update Pagination.js

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -10,26 +10,26 @@ export const Pagination = ({ prev, next, onPrevious, onNext }) => {
 
     return (
         <>
-        {/* Pagonation */}
-        <div className="container-pagination">
-            <nav>
-                <ul className="pagination">
-                    <li className="page-item">
-                        {
-                            prev ? 
-                            <button className="page-link" onClick={handlePrevious}>Previous</button>
-                            : <button className="page-link" disabled onClick={handlePrevious}>Previous</button>
-                        }
-                        {
-                            next ? 
-                            <button className="page-link" onClick={handleNext}>Next</button>
-                            :
-                            <button className="page-link" disabled onClick={handleNext}>Next</button>  
-                        }
-                    </li> 
-                </ul>
-            </nav>
-        </div> 
+            {/* Pagonation */}
+            <div className="container-pagination">
+                <nav>
+                    <ul className="pagination">
+                        <li className="page-item">
+                            {
+                                prev ? 
+                                <button className="page-link" onClick={handlePrevious}>Previous</button>
+                                : <button className="page-link" disabled onClick={handlePrevious}>Previous</button>
+                            }
+                            {
+                                next ? 
+                                <button className="page-link" onClick={handleNext}>Next</button>
+                                :
+                                <button className="page-link" disabled onClick={handleNext}>Next</button>  
+                            }
+                        </li> 
+                    </ul>
+                </nav>
+            </div> 
         </>
     );
 }
