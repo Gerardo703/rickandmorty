71a82d7439db8a0f0d1cec162662ceb4199502db
Gerardo Vega <54243381+Gerardo703@users.noreply.github.com>
2021-09-15 21:12:35 -0300

    *
    *

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ export const Card = ({ characters }) => {
             {/* Card donde se muestra cada personaje */}
             <div className="container">
                 { characters.map( (character, id ) => (
-                    <div key={id} className="border-gradient border-gradient-green card">
+                    <div key={id} className="card">
                         <img className="card-body-img" src={character.image}  alt="" />
                         <div className="card-body">
                             {/* <img className="card-body-img" src={character.image} width="120" height="120" alt="" /> */}
